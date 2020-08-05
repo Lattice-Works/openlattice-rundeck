f7332da80af9bfe6465a6c6f8934c08099e47ccf
jokedurnez <joke.durnez@gmail.com>
2020-08-04 17:21:44 -0700

    apitools puts an unnecessary comma if no parameters

--- client_generation/R/R/incomplete_file_type.R
+++ client_generation/R/R/incomplete_file_type.R
@@ -17,7 +17,7 @@
 IncompleteFileType <- R6::R6Class(
   'IncompleteFileType',
   public = list(
-    initialize = function(, ...){
+    initialize = function(...){
       local.optional.var <- list(...)
     },
     toJSON = function() {
